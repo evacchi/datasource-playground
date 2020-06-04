0727afa656f9d00614d2d599b8f7467dc7b58d80
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-06-04 16:03:27 +0200

    add ordering

--- src/main/java/org/kie/playground/ListDataStore.java
+++ src/main/java/org/kie/playground/ListDataStore.java
@@ -4,28 +4,32 @@ import java.util.ArrayList;
 import java.util.IdentityHashMap;
 
 public class ListDataStore<T> implements DataStore<T> {
-    private final IdentityHashMap<Object, DataHandle<?>> store = new IdentityHashMap<>();
+    private final IdentityHashMap<Object, DataHandle<T>> store = new IdentityHashMap<>();
+    private final ArrayList<DataHandle<T>> order = new ArrayList<>();
     private final ArrayList<Subscriber<T>> subscribers = new ArrayList<>();
 
     public DataHandle<T> add(T t) {
         DataHandle<T> dh = DataHandle.of(t);
         store.put(t, dh);
+        order.add(dh);
         subscribers.forEach(s -> s.added(dh));
         return dh;
     }
 
     @Override
     public void remove(T object) {
-        remove((DataHandle<T>) store.get(object));
+        remove(store.get(object));
     }
 
     public void remove(DataHandle<T> handle) {
         store.remove(handle.getObject());
+        order.remove(handle);
         subscribers.forEach(s -> s.removed(handle));
     }
 
     public void subscribe(Subscriber<T> subscriber) {
         subscribers.add(subscriber);
+        order.forEach(subscriber::added);
     }
 
 }
