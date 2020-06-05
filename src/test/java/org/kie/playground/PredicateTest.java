dc36751ecdafb171667562e0d116c0a60ccd4483
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-06-05 10:56:04 +0200

    predicates

--- src/test/java/org/kie/playground/PredicateTest.java
+++ src/test/java/org/kie/playground/PredicateTest.java
@@ -2,15 +2,17 @@ package org.kie.playground;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PredicateTest {
+
     @Test
-    public void checkContents() {
+    public void filter1() {
         var ds = new ListDataStream<String>();
-        // prepend "x" to all strings it sees
+        // keep vowels
         var f = PredicateFilter.of((String s) -> s.matches("[aeiouAEIOU]"));
 
         ds.subscribe(f);
@@ -22,7 +24,38 @@ public class PredicateTest {
         ds.append("b");
         ds.append("c");
 
-
         assertEquals(List.of("a"), rec.getData());
+    }
+
+    /**
+     * strings
+     * /  \
+     * vowels  non-vowels
+     */
+    @Test
+    public void filter2() {
+        var ds = new ListDataStream<String>();
+
+        // keep vowels
+        var f1 = PredicateFilter.of((String s) -> s.matches("[aeiouAEIOU]"));
+        // keep others
+        var f2 = PredicateFilter.of((String s) -> s.matches("[^aeiouAEIOU]"));
+
+        ds.subscribe(f1);
+        ds.subscribe(f2);
+
+        var rec1 = new RecordingSubscriber<String>();
+        f1.subscribe(rec1);
+
+        var rec2 = new RecordingSubscriber<String>();
+        f2.subscribe(rec2);
+
+        ds.append("a");
+        ds.append("b");
+        ds.append("c");
+        ds.append("1");
+
+        assertEquals(List.of("a"), rec1.getData());
+        assertEquals(List.of("b", "c", "1"), rec2.getData());
     }
 }
