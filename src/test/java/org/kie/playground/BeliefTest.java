168c6793da446999ea3903698a4dec088737b1e1
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-06-11 17:24:50 +0200

    test

--- src/test/java/org/kie/playground/BeliefTest.java
+++ src/test/java/org/kie/playground/BeliefTest.java
@@ -7,21 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BeliefTest {
+
     @Test
     public void checkContents() {
-        var hello1 = DataHandle.of("Hello");
-        var hello2 = DataHandle.of("Hello");
-        var hi1 = DataHandle.of("Hi");
-        var hi2 = DataHandle.of("Hi");
+        var stated = new ListDataStore<String>();
+        var justified = new ListDataStore<String>();
+
+        var hello1 = "Hello";
+        var hello2 = "Hello";
+        var hi1 = "Hi";
+        var hi2 = "Hi";
 
         var ds = new Belief<String>();
         var rec = new RecordingSubscriber<String>();
         ds.subscribe(rec);
 
-        ds.stated().added(hello1);
-        ds.stated().added(hello2);
-        ds.justified().added(hi1);
-        ds.justified().added(hi2);
+        stated.subscribe(ds.stated());
+        justified.subscribe(ds.justified());
+        stated.add(hello1);
+        stated.add(hello2);
+        justified.add(hi1);
+        justified.add(hi2);
 
         assertEquals(List.of("Hello", "Hello", "Hi"), rec.getData());
     }
